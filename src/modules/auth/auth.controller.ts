0da3e1c4142e7f8463c17de978d7933e6e2a2e39
Tohir Ravshanov <rth@MacBook-Pro-Tohir.local>
2025-07-28 16:23:32 +0500

    fx

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,40 @@
 import { AuthService } from './auth.service';
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
 import { LoginDto, RegisterDto, ConfirmEmailDto, DeviceFcmTokenUpdateDto } from './dto';
+import { ApiOperation, ApiProperty } from '@nestjs/swagger';
+import { IRequest } from '@interfaces';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Register mobile', description: 'Register mobile' })
+  @Post('register')
+  async register(data: RegisterDto) {
+    return await this.authService.register(data);
+  }
+
+  @ApiOperation({ summary: 'Login mobile', description: 'Login mobile' })
+  @Post('login')
+  async login(data: LoginDto) {
+    return await this.authService.login(data);
+  }
+
+  @ApiOperation({ summary: 'Get me mobile', description: 'Get me mobile' })
+  @Get('me-user')
+  async getMeUser(@Req() request: IRequest) {
+    return await this.authService.getMeUser(request.user.id);
+  }
+
+  @ApiOperation({ summary: 'Update Fcm Token mobile', description: 'Update Fcm Token mobile' })
+  @Patch('device/fcm-token')
+  async updateDeviceFcmToken(@Req() request: IRequest, data: DeviceFcmTokenUpdateDto) {
+    return await this.authService.deviceFcmTokenUpdate(request.user.id, data);
+  }
+
+  @ApiOperation({ summary: 'Get me admin', description: 'Get me admin' })
+  @Get('me-staff')
+  async getMeStaff(@Req() request: IRequest) {
+    return await this.authService.getMeStaff(request.user.id);
+  }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, HttpStatus, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '@prisma';
 import * as bcrypt from 'bcrypt';
-import { RegisterDto } from './dto';
+import { RegisterDto, DeviceFcmTokenUpdateDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -66,6 +66,65 @@ export class AuthService {
     return {
       status: HttpStatus.CREATED,
       message: 'Пользователь успешно создан!',
+    };
+  }
+
+  async getMeUser(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        is_verified: true,
+        created_at: true,
+      },
+    });
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Пользователь успешно получен!',
+      data: user,
+    };
+  }
+
+  async getMeStaff(id: number) {
+    const staff = await this.prisma.staff.findUnique({
+      where: {
+        id: id,
+      },
+      select: {
+        id: true,
+        name: true,
+        login: true,
+        status: true,
+        created_at: true,
+      },
+    });
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Пользователь успешно получен!',
+      data: staff,
+    };
+  }
+
+  async deviceFcmTokenUpdate(userId: number, fcmToken: DeviceFcmTokenUpdateDto) {
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        fcm_token: fcmToken.fcm_token,
+      },
+    });
+
+    return {
+      status: HttpStatus.OK,
+      message: 'Токен FCM успешно обновлен!!',
+      data: null,
     };
   }
 }
