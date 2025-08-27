9d90083ca12ed91cc344aa003cb74bb2a2969762
rthcoder <tohirdavrsmart@gmail.com>
2025-08-27 17:01:48 +0500

    fx

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -479,7 +479,7 @@ export class OrderService {
     console.log(productCode, 'productCode');
     console.log(updatedOrder, 'updatedOrder for qr order');
 
-    await this.joyTel.orderQrCode(order.product_code, firstSn.snCode, 1, order.order_tid);
+    await this.joyTel.orderQrCode(updatedOrder.product_code, firstSn.snCode, 1, updatedOrder.order_tid);
 
     return {
       code: '000',
