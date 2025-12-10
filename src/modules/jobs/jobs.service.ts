a2da060e3d70bbb33829bbd34afc29677f995d86
rthcoder <tohirdavrsmart@gmail.com>
2025-12-10 14:36:47 +0500

    fx

--- src/modules/jobs/jobs.service.ts
+++ src/modules/jobs/jobs.service.ts
@@ -1,3 +1,4 @@
+import { PartnerIds } from '@enums';
 import { BillionConnectService, JoyTel } from '@http';
 import { WinstonLoggerService } from '@logger';
 import { Injectable } from '@nestjs/common';
@@ -19,31 +20,45 @@ export class JobsService {
     this.logger.log('Joy Tel Orders Checker CRON is working!');
   }
 
-  // @Cron(CronExpression.EVERY_5_MINUTES)
-  // async checkSimStatusOnPartnerSide() {
-  //   this.logger.log('Sim Status on partner side CRON is working!');
-  //   const sims = await this.prisma.sims.findMany({
-  //     where: {
-  //       status: OrderStatus.COMPLETED,
-  //       sim_status: {
-  //         notIn: [SimStatus.ACTIVATED, SimStatus.EXPIRED],
-  //       },
-  //       coupon: {
-  //         not: null,
-  //       },
-  //       iccid: {
-  //         not: null,
-  //       },
-  //     },
-  //     select: {
-  //       id: true,
-  //       coupon: true,
-  //       partner_id: true,
-  //       channel_order_id: true,
-  //       iccid: true,
-  //     },
-  //   });
-  // }
+  @Cron(CronExpression.EVERY_10_SECONDS)
+  async checkSimStatusOnPartnerSide() {
+    this.logger.log('Sim Status on partner side CRON is working!');
+    const sims = await this.prisma.sims.findMany({
+      where: {
+        status: OrderStatus.COMPLETED,
+        sim_status: {
+          notIn: [SimStatus.ACTIVATED, SimStatus.EXPIRED],
+        },
+        iccid: {
+          not: null,
+        },
+      },
+      select: {
+        id: true,
+        coupon: true,
+        partner_id: true,
+        channel_order_id: true,
+        iccid: true,
+      },
+    });
+
+    if (!sims || sims.length === 0) {
+      this.logger.log('Sims not found for update status');
+      return;
+    }
+
+    for (let sim of sims) {
+      if (sim.partner_id === PartnerIds.JOYTEL) {
+        const response = await this.joyTelService.getStatus({ coupon: sim?.coupon });
+        console.log('Joytel check status response: ', response);
+      }
+      if (sim.partner_id === PartnerIds.BILLION_CONNECT) {
+        const response = await this.billionConnectService.getStatus({ iccid: sim?.iccid });
+        console.log('BC CHECK status cron response: ', response);
+      }
+    }
+    this.logger.info('Finish update partner status in partner side');
+  }
 
   // @Cron(CronExpression.EVERY_12_HOURS)
   // async updateBalance() {
