18403e5c313fc267e5d5f39fdc76c28a3e36e7ac
rthcoder <tohirdavrsmart@gmail.com>
2025-12-10 12:22:36 +0500

    fx

--- src/common/helpers/date-converter.helper.ts
+++ src/common/helpers/date-converter.helper.ts
@@ -2,19 +2,19 @@ import { BadRequestException } from '@nestjs/common';
 
 export function dateConverter(date: string) {
   try {
-    const result = date.split('_');
+    const result = date?.split('_');
     const start = new Date(result[0]);
     const end = new Date(result[1]);
 
     // if (!(start && end && !isNaN(start.getTime()) && !isNaN(end.getTime()))) {
     //   throw new Error();
     // }
-    start.setHours(0, 0, 0, 0);
-    end.setHours(23, 59, 59, 999);
+    start?.setHours(0, 0, 0, 0);
+    end?.setHours(23, 59, 59, 999);
 
     return {
-      startDate: start,
-      endDate: end,
+      startDate: start ?? undefined,
+      endDate: end ?? undefined,
     };
   } catch (error) {
     throw new BadRequestException('Неверный тип даты!');
