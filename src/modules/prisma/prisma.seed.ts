b830245fd0a0e2fe3563caf71cd5b347c38fda65
Tohir Ravshanov <rth@MacBook-Pro-Tohir.local>
2025-12-23 13:07:36 +0500

    fx

--- src/modules/prisma/prisma.seed.ts
+++ src/modules/prisma/prisma.seed.ts
@@ -4,10 +4,10 @@ const prisma = new PrismaClient();
 async function changeIncrementIdOfTransactionSeed() {
   await prisma.$executeRawUnsafe(`
     SELECT setval(
-      'Transaction_id_seq',
+      'transaction_id_seq',
       GREATEST(
         1000,
-        (SELECT COALESCE(MAX(id), 0) FROM "Transaction") + 1
+        (SELECT COALESCE(MAX(id), 0) FROM transaction) + 1
       )
     );
   `);
